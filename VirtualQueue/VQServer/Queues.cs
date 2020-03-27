bd62b8367e3eeb0edd71d8f47c37506a95ba7c29
Martin Ganev <marty.ganev@gmail.com>
2020-03-28 00:05:57 +0200

    Added model classes for db tables in server proj

---
+++ VirtualQueue/VQServer/Queues.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VQServer
+{
+    public class Queues
+    {
+        public int queueId { get; set; }
+        public int placeName { get; set; }
+        public int locationName { get; set; }
+        public int managerId { get; set; }
+    }
+}
---
+++ VirtualQueue/VQServer/Users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VQServer
+{
+    public class Users
+    {
+        public int usersId { get; set; }
+        public int MAC { get; set; }
+        public int queueId { get; set; }
+    }
+}
