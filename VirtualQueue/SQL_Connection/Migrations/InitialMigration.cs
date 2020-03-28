7f7f1965ac37481e36b04affe0a56499ee01704e
DeqnBerberov <35681800+DeqnBerberov@users.noreply.github.com>
2020-03-28 12:53:32 +0200

    Updated Database

--- VirtualQueue/SQL_Connection/Migrations/InitialMigration.cs
+++ VirtualQueue/SQL_Connection/Migrations/InitialMigration.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
