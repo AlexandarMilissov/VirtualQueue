7df50805906069e14043d480de0762198f5a4e5c
Martin Ganev <marty.ganev@gmail.com>
2020-03-28 18:56:04 +0200

    TempCommit

--- VirtualQueue/VirtualQueue/VirtualQueue.Android/obj/Debug/90/android/environment.x86_64.s
+++ VirtualQueue/VirtualQueue/VirtualQueue.Android/obj/Debug/90/android/environment.x86_64.s
@@ -69,7 +69,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"ca9e7b0d-db40-4a4b-bb4c-fbcfed69da98"
+	.asciz	"326ae39d-563e-48dc-bcb0-56c8a7a94802"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
